4fcad64fd32e5b144150d840ec403caf7902d7f0
Adam Baker <adamjamesbaker@gmail.com>
2020-10-14 10:34:10 +0100

    Green - Add deposit function

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,7 +1,16 @@
 class PiggyBank
 
-  def initialize
+  attr_reader :coins
 
+  def initialize
+    @coins = 0
+  end
+
+
+  attr_writer :coins
+  
+  def deposit(num)
+    @coins += num
   end
 
 end
